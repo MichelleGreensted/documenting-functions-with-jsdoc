43622f2336fc96dc532fc7df792066f4c702f5ee
MichelleG <michellegreensted@gmail.com>
2021-06-09 10:57:32 +0100

    Write function in challenge folder

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,15 @@
 // TODO (Ex.0): document and write tests for this function
 
+/**
+ * When given a number, returns the negative version of the number.
+ * @param {number} n - input number
+ * @returns {number} negative version of n
+ */
 function asNegative(n) {
-  // ...
+  n2 = n * -1
+  return n2
 }
 
-
+console.log(asNegative(4))
+console.log(asNegative(-3))
+console.log(asNegative(0))
